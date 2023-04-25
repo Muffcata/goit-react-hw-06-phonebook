94f8b1ca2b06c482ad4c03d4c8375bde6e3f1c85
Muffcata <marta.oched@gmail.com>
2023-04-25 20:13:28 +0200

    to do local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,8 @@ import React from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './Contacts/Contacts';
 import Filter from './Filter/Filter';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { getContacts } from 'redux/selectors';
 
 export const App = () => {
-  const contacts = useSelector(getContacts);
-  useEffect(() => {
-    if (contacts !== []) {
-      localStorage.setItem('CONTACTS', JSON.stringify(contacts));
-    }
-  }, [contacts]);
   return (
     <div>
       <h1>Phonebook</h1>
